599f946ecd567b5c1c8b5739f5b8209361249091
Ivan <malyshev.ivan@gmail.com>
2025-12-15 18:23:26 +0100

    Info label text

---
+++ src/info_label.gd
@@ -0,0 +1,30 @@
+class_name InfoLabel
+extends RichTextLabel
+
+const URLS := {
+	"issues": "https://github.com/0-Vanes-0/DD2-Squad-Builder/issues",
+	"Steam": "https://store.steampowered.com/app/1940340/Darkest_Dungeon_II/",
+	"Godot": "https://godotengine.org/",
+}
+@export var notification_panel: NotificationPanel
+
+
+func _ready() -> void:
+	assert(notification_panel)
+	self.text = self.text % ProjectSettings.get_setting("application/config/version")
+
+
+func _on_meta_clicked(meta: Variant) -> void:
+	OS.shell_open(URLS[str(meta)])
+
+
+func _on_meta_hover_started(meta: Variant) -> void:
+	notification_panel.show_message(URLS[str(meta)], true, false)
+
+
+func _on_meta_hover_ended(_meta: Variant) -> void:
+	notification_panel.hide()
+
+
+func _on_mouse_exited() -> void:
+	notification_panel.hide()
--- src/notification_panel.gd
+++ src/notification_panel.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 
 const DURATION_PER_CHARACTER := 0.1 # seconds
 @export var label: Label
+var is_on_right := true
 
 
 func _ready() -> void:
@@ -13,10 +14,15 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	var mouse_pos := get_viewport().get_mouse_position()
 	self.position = mouse_pos + Vector2.UP * self.size
+	if not is_on_right:
+		self.position += Vector2.LEFT * self.size
 
 
-func show_message(message: String):
+func show_message(message: String, is_hover := false, on_right := true):
+	is_on_right = on_right
 	label.text = " " + message + " "
+	self.size.x = 1
 	self.show()
-	await get_tree().create_timer(DURATION_PER_CHARACTER * message.length()).timeout
-	self.hide()
+	if not is_hover: # is lick
+		await get_tree().create_timer(DURATION_PER_CHARACTER * message.length()).timeout
+		self.hide()
